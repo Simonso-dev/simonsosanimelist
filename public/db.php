2ffd0c571d09705060a54b0d0f08580ff5bb2275
Simonso <wantedsimonso@gmail.com>
2021-03-18 23:17:47 +0100

    fix

--- public/db.php
+++ public/db.php
@@ -1,10 +1,10 @@
 <?php
 $url = parse_url(getenv("mysql://b5b71d88dff84a:40f8dada@eu-cdbr-west-03.cleardb.net/heroku_bec9e21fd1904b0?reconnect=true"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+$server = $url["eu-cdbr-west-03.cleardb.net"];
+$username = $url["b5b71d88dff84a"];
+$password = $url["40f8dada"];
+$db = substr($url["heroku_bec9e21fd1904b0"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
 ?>
