48da40d8e695e71d9449c35d2efde79d5642128c
Simonso <wantedsimonso@gmail.com>
2021-03-18 23:05:27 +0100

    fix

--- public/index.php
+++ public/index.php
@@ -17,9 +17,33 @@ include("header.php");
 
 
 <?php 
-include("registerAnime.php");
 include("readAnimelist.php");
+include("db.php");
 
 
-include("footer.php")
+if (isset($_POST["add"]))
+{
+    $animenr=$_POST["animenr"];
+    $animename=$_POST["animename"];
+    $episodes=$_POST["episodes"];
+    $score=$_POST["score"];
+
+    $legalAnimename=validateAnimename($animename);
+    $legalEpisodes=validateEpisodes($episodes);
+    $legalScore=validateScore($score);
+
+}
+     if($legalAnimename && $legalEpisodes && $score)
+      {
+
+        $sqlQuery="INSERT INTO Anime (Animename, Episodes, Score)
+                    VALUES ('$animename','$episodes','$score');";
+
+        $sqlResult=mysqli_query($conn, $sqlQuery);
+
+        
+        print("Anmie has been registered");
+
+      }
+include("footer.php");
 ?>
