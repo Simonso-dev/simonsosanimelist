782ef3293815ca4969ffa00822578bbdfdf4c2fa
Simonso <wantedsimonso@gmail.com>
2021-03-19 22:43:35 +0100

    fix

--- public/db.php
+++ public/db.php
@@ -9,8 +9,6 @@ $db = substr($url["heroku_bec9e21fd1904b0"], 1);
 $conn = new mysqli($server, $username, $password, $db) or die("Not able to contact the database-server");
 
 
-console_log($conn);
-
 function console_log( $data ){
     echo '<script>';
     echo 'console.log('. json_encode( $data ) .')';
--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 include("header.php");
+console_log($conn);
 ?>
                 
 
