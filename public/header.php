5b07e109d56f11912513267f215ac7d6abc5a3cd
Simonso <wantedsimonso@gmail.com>
2021-06-02 12:49:53 +0200

    fix

--- public/header.php
+++ public/header.php
@@ -10,15 +10,34 @@ if(!$loggedinUser) {
 <!DOCTYPE html>
 <html lang="en">
   <head>
+
+    <link rel="manifest" href="manifest.json">
+
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="application-name" content="SAL">
+    <meta name="apple-mobile-web-app-title" content="SAL">
+    <meta name="theme-color" content="#FFFFFF">
+    <meta name="msapplication-navbutton-color" content="#FFFFFF">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="msapplication-starturl" content="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="icon" type="image/png" sizes="192x192" href="/sal/public/images/SAL LOGO v2.png">
+    <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="/sal/public/images/SAL LOGO v2.png">
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="manifest" href="/manifest.json">
+
+
     <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="styles/addAnime.css">
     <link rel="stylesheet" href="styles/deleteAnime.css">
 
     <title>Simonso's Anime List</title>
     <link rel="icon" src="/sal/public/images/SAL LOGO v2.jpg">
+
+    <script src="registerSW.js"></script>
   </head>
          <body>
             <div class="container">
--- public/index.php
+++ public/index.php
@@ -1,19 +1,8 @@
 <?php
 include("header.php");
 ?>
-<script>
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sal/public/service-worker.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
-  }
-</script>
+
+
 <?php
 include("readAnimelist.php");
 
