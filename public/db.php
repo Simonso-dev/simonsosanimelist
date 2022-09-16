8748b7e5f7aa3f42ec2d3980ea3d65553070a89b
Simonso-dev <72812314+Simonso-dev@users.noreply.github.com>
2022-09-16 06:14:41 +0200

    removed db credentials

--- public/db.php
+++ public/db.php
@@ -1,10 +1,10 @@
 <?php
 $url = parse_url(getenv("mysql://ofe7c7506pnk3swy:z4ypie1ranjpaa1x@eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/o9d6d8m1y5kentde"));
 
-$server = "eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-$username = "ofe7c7506pnk3swy";
-$password = "z4ypie1ranjpaa1x";
-$db = "o9d6d8m1y5kentde";
+$server = "";
+$username = "";
+$password = "";
+$db = "";
 
 $conn = new mysqli($server, $username, $password, $db) or die("Not able to contact the database-server");
 ?>
