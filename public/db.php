13de79223509c1390c8b6586074c2595983f5cae
Simonso-dev <72812314+Simonso-dev@users.noreply.github.com>
2023-02-22 13:31:32 +0100

    db fix

--- public/db.php
+++ public/db.php
@@ -1,5 +1,5 @@
 <?php
-$url = parse_url(getenv("mysql://ofe7c7506pnk3swy:z4ypie1ranjpaa1x@eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/o9d6d8m1y5kentde"));
+$url = parse_url(getenv(""));
 
 $server = "";
 $username = "";
